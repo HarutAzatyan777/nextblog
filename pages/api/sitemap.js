406825a42191a9dd8a895d3ec8180d78fd58daa6
sky <harutazatyan45@gmail.com>
2024-09-26 14:12:35 +0400

    add sitemap-1

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,50 +1,33 @@
-import { getPosts, getCategories } from '../../services'; // Ensure the path to services is correct
+import { getCategories, getPosts } from '../../services'; // Adjust the path if necessary
 
-const Sitemap = () => {};
+const generateSitemap = (posts, categories) => {
+  const postUrls = posts.map(({ node }) => {
+    return `<url>
+      <loc>${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${node.slug}`}</loc>
+      <lastmod>${new Date(node.createdAt).toISOString()}</lastmod>
+    </url>`;
+  });
 
-export const getServerSideProps = async ({ res }) => {
+  const categoryUrls = categories.map(({ slug }) => {
+    return `<url>
+      <loc>${`${process.env.NEXT_PUBLIC_SITE_URL}/category/${slug}`}</loc>
+    </url>`;
+  });
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+    ${postUrls.join('')}
+    ${categoryUrls.join('')}
+  </urlset>`;
+};
+
+export default async function Sitemap(req, res) {
   const posts = await getPosts();
   const categories = await getCategories();
 
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
-
-  // Create the XML structure
-  const sitemap = `
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
-      <url>
-        <loc>${baseUrl}</loc>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-      </url>
-      ${posts
-        .map(({ node }) => `
-          <url>
-            <loc>${`${baseUrl}/post/${node.slug}`}</loc>
-            <changefreq>weekly</changefreq>
-            <priority>0.7</priority>
-            <lastmod>${new Date(node.createdAt).toISOString()}</lastmod>
-          </url>
-        `)
-        .join('')}
-      ${categories
-        .map(({ slug }) => `
-          <url>
-            <loc>${`${baseUrl}/category/${slug}`}</loc>
-            <changefreq>monthly</changefreq>
-            <priority>0.5</priority>
-          </url>
-        `)
-        .join('')}
-    </urlset>
-  `;
+  const sitemap = generateSitemap(posts, categories);
 
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
   res.end();
-
-  return {
-    props: {},
-  };
-};
-
-export default Sitemap;
+}
